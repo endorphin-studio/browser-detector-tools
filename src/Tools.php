ac0b412bf0b8b905a9142d8b4adebe8f60182001
Thomas Müller <thomas.mueller@geld.de>
2020-06-02 13:02:03 +0200

    Fix regex error

--- src/Tools.php
+++ src/Tools.php
@@ -77,7 +77,7 @@ class Tools
      */
     public static function getVersionPattern(string $phrase): string
     {
-        return sprintf('/%s(\/| )[\w-._]{1,15}/', $phrase);
+        return sprintf('/%s(\/| )[\w\-._]{1,15}/', $phrase);
     }
 
     /**
